8c576095ff22a26effb0a90bf1f4b6aea0fdd975
matt <mtriantafellu@yahoo.com>
2017-05-23 17:51:30 -0400

    correctly got js file to work

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// Lets start by grabbing a reference to the <span> below:
+var userText = document.getElementById("user-text");
+
+
+//Next, we give JavaScript a function to execute when onkeyup event fires.
+document.onkeyup = function(event) {
+	console.log('event', event);
+	userText.textContent = event.key;
+}
