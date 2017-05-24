7216d0d2cc927afd282617c0a7845b93f0b33b1b
matt <mtriantafellu@yahoo.com>
2017-05-23 20:55:10 -0400

    trying new javascript code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,43 @@
 // Lets start by grabbing a reference to the <span> below:
 var userText = document.getElementById("user-text");
 
-
 //Next, we give JavaScript a function to execute when onkeyup event fires.
+
 document.onkeyup = function(event) {
 	console.log('event', event);
 	userText.textContent = event.key;
 }
+
+document.onkeyup = function(event) {
+	
+	var planetMars = ["m", "a", "r", "s"]
+	var userGuess = event.key.toLowerCase().charAt(0);
+
+	for (var i = 0; i < 20; i++) {
+
+	if (userGuess === "planetMars[1]" ||
+		userGuess === "planetMars[2]" ||
+		userGuess === "planetMars[3]" ||
+		userGuess === "planetMars[4]" || ) {
+
+
+
+		}
+	}
+
+
+
+
+
+function letterChecker(x) {}
+
+	var firstChar = x.tolowerCase().charAt(0);
+
+	if (firstChar === "planetMars[1]" || 
+		firstChar === "planetMars[2]" ||
+
+
+
+
+
+
